3393de5a16c9502cf274610b47bb9bcb7c5fc690
Emil Tullstedt <sakjur@gmail.com>
2015-01-24 22:24:37 +0100

    3.5 Made own statPrint

--- lab1/stat.c
+++ lab1/stat.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-extern void statPrint(char *, struct stat *);
+#define TIMEFORMATBUFFER 127
+
+void statPrint(char *, struct stat *);
 
 int main(int argc, char* argv[]) {
     struct stat buf;
@@ -21,5 +24,18 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+}
+
+void statPrint(char * file, struct stat * fileinfo) {
+    printf("File: %s\n", file);
+    printf("Size: %-8u Blocks: %-8u I/O Block: %-8u\n",
+        fileinfo->st_size, fileinfo->st_blocks, fileinfo->st_blksize);
+    printf("Device: %#010x Inode: %-8u Links: %-8u\n",
+        fileinfo->st_dev, fileinfo->st_ino, fileinfo->st_nlink);
+    printf("Access: (%06o /) UID: ( %u/ ) GID: ( %u/ )\n",
+        fileinfo->st_mode, fileinfo->st_uid, fileinfo->st_gid);
+    printf("Access: %s", ctime(&fileinfo->st_atim.tv_sec));
+    printf("Modified: %s", ctime(&fileinfo->st_mtim.tv_sec));
+    printf("Created: %s", ctime(&fileinfo->st_ctim.tv_sec));
 }
 
