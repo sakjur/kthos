d294da852130bd9e26ec76b06cac19591fce6648
Emil Tullstedt <sakjur@gmail.com>
2015-01-26 23:38:06 +0100

    Added Hello, World in nasm

---
+++ lab1/hello_nasm.s
@@ -0,0 +1,17 @@
+section data
+    string: db  "Hello, World!", 0xA, 0
+
+section text
+
+global _start
+_start:
+    mov rax, 1 ; write
+    mov rdi, 1 ; stdout
+    mov rsi, string
+    mov rdx, 14 ; length of string
+    syscall
+    mov rax, 60
+    mov rdi, 0
+    syscall
+    ret
+
