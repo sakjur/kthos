2e441bded7af4f2a54cf87c5e39ab406cadbb7ee
Emil Tullstedt <sakjur@gmail.com>
2015-02-03 17:41:29 +0100

    Tree in C done. Was supposed to be a bashscript :sob:

--- lab2/mytree.c
+++ lab2/mytree.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define STRBUFFER 2048
+#define TYPE_DIR 4
 
 void mytree(char *, int);
 
@@ -15,30 +16,41 @@ void main(int argc, char * argv[]) {
     else
         strncpy(buf, argv[1], STRBUFFER);
 
-    printf(".\n");
+    printf("%s\n.\n", buf);
     mytree(buf, 1);
 }
 
 void mytree(char * directory, int level) {
     struct dirent * fileentry; 
 
-    DIR *  currdir = opendir(directory);
-    char * dirname = strcat(directory, "/");
-    
-    if (!currdir) return;
+    DIR  * currdir = opendir(directory);
+    char dirname[STRBUFFER];
+    strncpy(dirname, directory, STRBUFFER);
+    strcat(dirname, "/");
+
+    if (!currdir)
+        return;
 
     while(fileentry = readdir(currdir)) {
+        char tmpdir[STRBUFFER];
+        strncpy(tmpdir, dirname, STRBUFFER);
+        
         if (!strncmp(fileentry->d_name, ".", 1))
             continue;
         for (int i = level; i > 0; i--)
             if (i == 1)
                 printf("-");
             else
-                printf("+");
-        printf(" %s\n", fileentry->d_name);
+                printf("|");
 
-        if (fileentry->d_type == 4) {
-            mytree(strcat(dirname, fileentry->d_name), level + 1);
+        if (fileentry->d_type == TYPE_DIR) {
+            printf(" %s/\n", fileentry->d_name);
+            mytree(strcat(tmpdir, fileentry->d_name), level + 1);
+        } else {
+            printf(" %s\n", fileentry->d_name);
         }
     }
+
+    closedir(currdir);
 }
+
