6d49a06f6e0d146820c47e740fe72837b20cc1ea
Emil Tullstedt <sakjur@gmail.com>
2015-02-03 23:10:50 +0100

    Added comments

--- lab2/tree.sh
+++ lab2/tree.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-if [ $1 = "" ]; then
-    FOLDER=`pwd`;
-else
-    FOLDER=$1;
-fi
-
-echo $FOLDER;
-
 function printout {
     # I used for, but changed to while since `for` breaked on space
     # while `while` breaks on new line, which allows for spaces in
     # filenames
+
+    # $1 is the current folder
+    # $2 is the depth of the current folder
+    # Bash doesn't seem to like local variables, so not giving them
+    # human-friendly names
 
     ls -p "$1" | while read FILE
     do
@@ -21,12 +18,19 @@ function printout {
         done
 
         echo $FILE;
-        if [ ${FILE: -1} = "/" ]; then
+        if [ ${FILE: -1} = "/" ]; then # Last character of variable
             NEWFOLDER=$1"/"$FILE;
             printout "$NEWFOLDER" "$2 1";
         fi
     done
 }
+
+# On no argument, use current directory. Otherwise use first argument as dir
+if [ $1 = "" ]; then
+    FOLDER=`pwd`;
+else
+    FOLDER=$1;
+fi
 
 printout $FOLDER "1";
 
